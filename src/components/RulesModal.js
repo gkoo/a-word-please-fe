da5dffb30662e64c87d7d7dda4f3fcb6e424d705
Gordon Koo <gordon@opendoor.com>
2020-05-24 11:32:55 -0700

    rules modal text

--- src/components/RulesModal.js
+++ src/components/RulesModal.js
@@ -10,42 +10,83 @@ function RulesModal({ onClose, show }) {
         <Modal.Title>How to play</Modal.Title>
       </Modal.Header>
       <Modal.Body className='rules-modal-body'>
-        <p>Get your love to the Princess!</p>
-        <h5><u>Gameplay</u></h5>
         <p>
-          Each player starts with one random card.
-        </p>
-        <p>
-          On your turn, you draw a card from the deck. You
-          then play one card and keep the other. When you play your card, you apply the card's
-          effect. You can read about each card effect in the right column of the screen. Once you
-          have played the card, it becomes the next player's turn.
+          A Word Please? is a cooperative game where everyone works together to guess as many
+          mystery words as possible.
         </p>
 
-        <h5><u>Winning a Round</u></h5>
+        <h5><u>Turns</u></h5>
+
         <p>
-          A round ends when either there are no more cards left in the deck to draw. At that point,
-          the player with the highest card number in hand wins the round and earns a token.
+          Each turn, one player is the guesser and the other players are the clue givers. The
+          mystery word will be visible to the clue givers, but not to the guesser. The clue
+          givers each choose a one-word clue to help the guesser guess the word. If more than
+          one player chooses the same clue word, that clue is hidden from the guesser.
         </p>
+
         <p>
-          A round can also end if all but one players are knocked out of the round. If that happens,
-          the last player standing earns a token.
+          Once all clues have been submitted, the guesser sees all unique clues and has the
+          opportunity to guess the mystery word.
         </p>
-        <h6><u>Tiebreak</u></h6>
+
         <p>
-          If two players have the same number card at the end of the round, they each sum up the
-          numbers of the cards in their discard pile. The player with the highest sum wins the
-          round.
+          <em>If the guesser guesses correctly:</em> the group scores a point.
         </p>
-        <h5><u>Winning the Game</u></h5>
+
         <p>
-          The game ends when a player has reached a certain number of tokens, depending on the
-          number of players in the game.
+          <em>If the guesser guesses incorrectly:</em> the group loses a turn.
         </p>
+
         <p>
-          2 players: 7 tokens<br/>
-          3 players: 5 tokens<br/>
-          4 players: 4 tokens<br/>
+          <em>Skipping:</em> If the guesser is does not feel confident, he or she can skip the turn
+          without guessing.
+        </p>
+
+        <h5><u>Clues</u></h5>
+
+        <p>The following are all considered valid clues.</p>
+
+        <ul>
+          <li>Numbers and digits (007)</li>
+          <li>Acronyms (FBI)</li>
+          <li>Onomatopoeia (Meow)</li>
+          <li>Special characters ($)</li>
+        </ul>
+
+        <p>The following are all considered invalid clues.</p>
+
+        <ul>
+          <li>The mystery word</li>
+          <li>
+            The mystery word written in a foreign language. (If the mystery word
+            is <em>Friend</em>, your clue cannot be <em>Amigo</em>.)
+          </li>
+          <li>
+            A word that is phonetically identical to the mystery word, but whose
+            meaning is different. (For example, if the mystery word is <em>Oar</em>, your clue
+            cannot be <em>Or</em>.)
+          </li>
+          <li>Made up words.</li>
+          <li>
+            Variants of the same word family. (For example, if the mystery word
+            is <em>Prince</em>, your clue cannot be <em>Princess</em>.)
+          </li>
+          <li>
+            Variants of the same word. (Plurals, gender differentiations, and spelling
+            mistakes.)
+          </li>
+        </ul>
+
+        <h5><u>Honor System</u></h5>
+
+        <p>
+          Players are trusted to play by the rules. If you're really determined to cheat, there's
+          probably a way to do so, but the game won't be fun.
+        </p>
+
+        <h5><u>End of the Game</u></h5>
+        <p>
+          Each game starts with 13 turns. The game ends when there are no more turns left.
         </p>
       </Modal.Body>
       <Modal.Footer>
